5bbcbfe55e63fc281718b2939f6b860d62ed4b97
maxfromspace <81488744+maxfromspace@users.noreply.github.com>
2021-10-04 20:26:36 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var availableScreenWidth = window.screen.availWidth;
-    if (availableScreenWidth < 500){
-        document.getElementById( 'my_form' ).style.width = String(availableScreenWidth) +  "px";
-    }else{
-        document.getElementById( 'my_form' ).style.width = String(availableScreenWidth/2) +  "px";
-    }
-});
